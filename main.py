16cc6b4edcc4c41c0734b0d8e71c75196608cd05
KSReierson <ksreierson@gmail.com>
2016-11-21 16:48:48 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -11,69 +11,130 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 autoescape=True)
 
 userList = []
-
 class Login(webapp2.RequestHandler):
+    error = ""
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('login.html')
-        
-        template_values = {
-            'Test': 'tst'
-        }
-        
-        self.response.write(template.render(template_values))
-        
+        uNm = self.request.cookies.get("CurrentUser")
+        if uNm == "":
+            template = JINJA_ENVIRONMENT.get_template('login.html')
+            template_values = {
+                "user": uNm,
+                "error": error,
+            }
+            self.response.write(template.render(template_values))
+
+
+        else:
+            if (getAccount(uNm).aType = 's'):
+                self.redirect('/studentcenter')
+            else:
+                self.redirect('/instructorcenter')
+
     def post(self):
         uNm = self.request.get('uName')
         uPwd = self.request.get('uPass')
-        
+
         validAcc = False
-        
+
         for item in userList:
             if item.getName() == uNm and item.getPwd() == uPwd:
-                #print(repr(item.getaType())) # Note that the function, repr shows white space characters. I'm not sure where the \r\n is coming from, but this works for now
-                #print(item.getaType() == "'ui")
-                #if item.getaType() == "i\r\n":
-                #    print("Instr")
                 validAcc = True
-        
         if validAcc == False:
             self.redirect("/")
-        
-        if validAcc == True:
-            self.redirect("/messcenter?user="+uNm)
 
-class MessCenter(webapp2.RequestHandler):
+        if validAcc == True:
+            #self.response.set_cookie('name', name, path='/')
+            self.response.set_cookie("CurrentUser", uNm, max_age=360, path="/")
+            if (getAccount(uNm).aType = 's'):
+                self.redirect("/studentcenter")
+            else:
+                self.redirect("/instructorcenter")
+
+        else:
+            error = "enter a valid username and password"
+
+
+class InstructorCenter(webapp2.RequestHandler):
+
+    QL = Question.query(Question.lec=getAccount(uNm).lec).fetch()
+
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('messcenter.html')
-        
-        uNm = self.request.get("user")
+        uNm = self.request.cookies.get('CurrentUser')
+        QL = Question.query(Question.lec=getAccount(uNm).lec).fetch()
+        template = JINJA_ENVIRONMENT.get_template('instructorcenter.html')
+        uNm = self.request.get("CurrentUser")
         template_values = {
-            "user": uNm,
-            "userList": userList
+            "CurrentUser" = uNm,
+            'QL' = QL,
         }
-        
-        self.response.write(template.render(template_values))
-    
+
     def post(self):
-        student = self.request.get("studentName")
-        self.redirect("/chat?student=" + student +"?instr=" + self.request.get("user"))
+        time = datetime.datetime.now()
+        uNm = self.request.cookies.get('CurrentUser')
+
+    def goToChat(self):
+
+        template_values = {
+            'user'
+        }
+
+
+
+
+
+
+class StudentCenter(webapp2.RequestHandler):
+    def get(self):
+    def post(self):
+
 
 class Chat(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('chat.html')
-        
+
+        student = self.request.get("student")
+        user = self.request.cookies.get("CurrentUser")
+
+        if student == "":
+            student = getInstrAccount(userList).getName()
+
+        self.response.set_cookie("receiver", student, max_age=360, path="/")
+        messages = list(Message.query().order(Message.time, -Message.time))
+
         template_values = {
-            'Test': 'tst'
+            "user": user,
+            "student": student,
+            "messages": messages,
+            "size": len(messages)
         }
-        
+
         self.response.write(template.render(template_values))
-        
+
     def post(self):
-        pass
+        user = self.request.cookies.get("CurrentUser")
+
+        message = Message(time=datetime.datetime.now(), content=self.request.get("message"), sender=getAccount(user, userList), receiver=getAccount(self.request.cookies.get("receiver"), userList))
+
+        message.put()
+
+        self.redirect("/messcenter?user=" + user)
+
+class Test(webapp2.RequestHandler):
+    def get(self)
+        template = JINJA_ENVIRONMENT.get_template('test.html')
+        test1 = "test 1, make sure this is running"
+        test2 = "test 2, and loading template vals"
+        template_values = {
+            "test1": test1,
+            "test2": test2
+        }
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
 	('/', Login),
-	('/messcenter', MessCenter),
+    ('/studentcenter'), StudentCenter),
+    ('/instructorcenter'), InstructorCenter,
+	('/test', Test),
 	('/chat', Chat)
 ], debug=True)
 
