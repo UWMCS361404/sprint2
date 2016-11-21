21b2d91146dfaafaa98b07256e86634d63f42183
EdwardDickenson <EdwardDickenson@users.noreply.github.com>
2016-11-21 17:18:29 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,103 +1,74 @@
 import webapp2
 import jinja2
 import os
+import time
+import datetime
+import calendar
 
-from user import User
-from util import parseTxt
+from google.appengine.ext import ndb
+from user import *
+from util import *
+from message import *
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
 autoescape=True)
 
-userList = []
 class Login(webapp2.RequestHandler):
-    error = ""
     def get(self):
-        uNm = self.request.cookies.get("CurrentUser")
-        if uNm == "":
-            template = JINJA_ENVIRONMENT.get_template('login.html')
-            template_values = {
-                "user": uNm,
-                "error": error,
-            }
-            self.response.write(template.render(template_values))
+        template = JINJA_ENVIRONMENT.get_template('login.html')
+        
+        template_values = {
+            'Test': 'tst'
+        }
 
-
-        else:
-            if (getAccount(uNm).aType = 's'):
-                self.redirect('/studentcenter')
-            else:
-                self.redirect('/instructorcenter')
-
+        self.response.write(template.render(template_values))
+        
     def post(self):
         uNm = self.request.get('uName')
         uPwd = self.request.get('uPass')
-
+        
         validAcc = False
 
         for item in userList:
-            if item.getName() == uNm and item.getPwd() == uPwd:
+            if item.getName() == uNm.strip() and item.getPwd().strip() == uPwd:
                 validAcc = True
+                
         if validAcc == False:
             self.redirect("/")
-
+        
         if validAcc == True:
-            #self.response.set_cookie('name', name, path='/')
             self.response.set_cookie("CurrentUser", uNm, max_age=360, path="/")
-            if (getAccount(uNm).aType = 's'):
-                self.redirect("/studentcenter")
-            else:
-                self.redirect("/instructorcenter")
+            self.redirect("/messcenter?user="+uNm)
 
-        else:
-            error = "enter a valid username and password"
-
-
-class InstructorCenter(webapp2.RequestHandler):
-
-    QL = Question.query(Question.lec=getAccount(uNm).lec).fetch()
-
+class MessCenter(webapp2.RequestHandler):
     def get(self):
-        uNm = self.request.cookies.get('CurrentUser')
-        QL = Question.query(Question.lec=getAccount(uNm).lec).fetch()
-        template = JINJA_ENVIRONMENT.get_template('instructorcenter.html')
-        uNm = self.request.get("CurrentUser")
-        template_values = {
-            "CurrentUser" = uNm,
-            'QL' = QL,
-        }
-
-    def post(self):
-        time = datetime.datetime.now()
-        uNm = self.request.cookies.get('CurrentUser')
-
-    def goToChat(self):
+        template = JINJA_ENVIRONMENT.get_template('messcenter.html')
+        
+        uNm = self.request.get("user")
 
         template_values = {
-            'user'
+            "user": uNm,
+            "userList": userList,
         }
-
-
-
-
-
-
-class StudentCenter(webapp2.RequestHandler):
-    def get(self):
+        
+        self.response.write(template.render(template_values))
+    
     def post(self):
-
+        student = self.request.get("studentName")
+        self.redirect("/chat?student=" + student)
 
 class Chat(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('chat.html')
-
+        
         student = self.request.get("student")
         user = self.request.cookies.get("CurrentUser")
-
+        
         if student == "":
             student = getInstrAccount(userList).getName()
-
+            
         self.response.set_cookie("receiver", student, max_age=360, path="/")
         messages = list(Message.query().order(Message.time, -Message.time))
 
@@ -107,35 +78,39 @@ class Chat(webapp2.RequestHandler):
             "messages": messages,
             "size": len(messages)
         }
-
+        
         self.response.write(template.render(template_values))
-
+        
     def post(self):
         user = self.request.cookies.get("CurrentUser")
 
         message = Message(time=datetime.datetime.now(), content=self.request.get("message"), sender=getAccount(user, userList), receiver=getAccount(self.request.cookies.get("receiver"), userList))
-
+        
         message.put()
-
+        
         self.redirect("/messcenter?user=" + user)
-
-class Test(webapp2.RequestHandler):
-    def get(self)
-        template = JINJA_ENVIRONMENT.get_template('test.html')
-        test1 = "test 1, make sure this is running"
-        test2 = "test 2, and loading template vals"
+        
+class Faq(webapp2.RequestHandler):
+    def get(self): 
+        template = JINJA_ENVIRONMENT.get_template('faq.html')
+        user = self.request.cookies.get("CurrentUser")
+        
         template_values = {
-            "test1": test1,
-            "test2": test2
+            "user": user            
         }
+        
         self.response.write(template.render(template_values))
+        
+    def post(self):
+        user = self.request.cookies.get("CurrentUser")
+        self.redirect("/messcenter?user=" + user)
+        
+
+userList = parseTxt("accounts.csv")
 
 app = webapp2.WSGIApplication([
 	('/', Login),
-    ('/studentcenter'), StudentCenter),
-    ('/instructorcenter'), InstructorCenter,
-	('/test', Test),
-	('/chat', Chat)
+	('/messcenter', MessCenter),
+	('/chat', Chat),
+	('/faq', Faq)
 ], debug=True)
-
-userList = parseTxt("accounts.csv")
--- message.py
+++ message.py
@@ -8,45 +8,43 @@ class Message(ndb.Model):
     content = ndb.StringProperty()
     sender = ndb.StructuredProperty(User)
     receiver = ndb.StructuredProperty(User)
-
+    
     def setSender(self, sender):
         self.sender = sender
-
+        
     def getSender(self):
         return self.getSender()
-
+        
     def setReceiver(self, receiver):
         self.receiver = receiver
         
     def getReceiver(self, receiver):
         return self.getreceiver
-
+        
     def getSender(self):
         return self.sender
-
+        
     def messagesWith(self, user):
         query = Message.query()
-
+        
     def messagesFrom(self, user):
         query = Message.query()
         return (list(query.filter(Message.sender == user)))
-
+        
     def setReceiver(self, receiver):
         self.receiver = receiver
-
+        
     def getReceiver(self):
         return self.receiver
-
+        
     def setTime(self, time):
         self.time = time
-
+        
     def getTime(self):
         return self.time
-
+        
     def setContent(self, content):
         self.content = content
-
+        
     def getContent(self):
         return self.content
-Contact GitHub API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Status Help
